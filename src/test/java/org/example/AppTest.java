c1839ac5b3dedf2ea89b1a557eb3d188268263fb
muska <muskaan.sultana2002@gmail.com>
2024-03-06 18:23:38 -0500

    Change "this" to delegate

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -10,27 +10,28 @@ import junit.framework.TestSuite;
 public class AppTest 
     extends TestCase
 {
-   public void testStackPushandPop(){
-    MyStack<Integer> stack = new MyStack<>();
-    assertEquals(0, stack.size());
+    public void testStackPushandPop(){
+        MyStack<Integer> stack = new MyStack<>();
+        assertEquals(0, stack.size());
 
 
-}
-public void testPush(){
-    MyStack<Integer> stack = new MyStack<>();
-    stack.push(42);
-    assertEquals(Integer.valueOf(42), stack.get(0));
+    }
+    public void testPush(){
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(42);
+        assertEquals(Integer.valueOf(42), stack.get(0));
 
-    stack.push(99);
-    stack.push(100);
-    stack.push(250);
-    assertEquals(Integer.valueOf(250), stack.get(3));
-}
-public void testPop(){
-    MyStack<Integer> stack = new MyStack<>();
-    stack.push(42);
-    stack.push(55);
-    assertEquals(Integer.valueOf(55), stack.pop());
+        stack.push(99);
+        stack.push(100);
+        stack.push(250);
+        assertEquals(Integer.valueOf(250), stack.get(3));
+    }
+    public void testPop(){
+        MyStack<Integer> stack = new MyStack<>();
+        stack.push(42);
+        stack.push(55);
+        assertEquals(Integer.valueOf(55), stack.pop());
+
 
 
 }
