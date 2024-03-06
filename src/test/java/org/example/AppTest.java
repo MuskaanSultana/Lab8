dd256fa1875305f1148b1938582dfd589529416e
muska <muskaan.sultana2002@gmail.com>
2024-03-06 14:29:41 -0500

    Push and Pop is implemented

--- src/test/java/org/example/AppTest.java
+++ src/test/java/org/example/AppTest.java
@@ -13,8 +13,25 @@ public class AppTest
    public void testStackPushandPop(){
     MyStack<Integer> stack = new MyStack<>();
     assertEquals(0, stack.size());
-    stack.push(1);
-    assertEquals(1, stack.size());
+
+
+}
+public void testPush(){
+    MyStack<Integer> stack = new MyStack<>();
+    stack.push(42);
+    assertEquals(Integer.valueOf(42), stack.get(0));
+
+    stack.push(99);
+    stack.push(100);
+    stack.push(250);
+    assertEquals(Integer.valueOf(250), stack.get(3));
+}
+public void testPop(){
+    MyStack<Integer> stack = new MyStack<>();
+    stack.push(42);
+    stack.push(55);
+    assertEquals(Integer.valueOf(55), stack.pop());
+
 
 }
 }
